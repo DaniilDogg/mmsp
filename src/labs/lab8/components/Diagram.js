d5f3ceb89148d6fb721a36d6db0b4b3feb2a659d
DaniilDogg <yurchenko.daniil.2001@gmail.com>
2022-11-30 14:45:07 +0200

    deploy

---
+++ src/labs/lab8/components/Diagram.js
@@ -0,0 +1,53 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export const Diagram = ({ data, w }) => {
+  const chart = useRef(null);
+  const [height, setHeight] = useState(w / 2);
+
+  const resizeHandler = () => {
+    if (chart.current != null) {
+      setHeight(chart?.current?.offsetWidth / 2);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
+  if (data == null) return <></>;
+  let i = 0;
+  return (
+    <>
+      <div
+        className="container-fluid d-flex justify-content-center align-items-center mt-4 py-2 border border-secondary rounded border-opacity-50"
+        ref={chart}
+      >
+        <ResponsiveContainer width={"100%"} height={height}>
+          <LineChart data={data} margin={{ top: 10, right: 10, left: -35 }}>
+            <Line type="monotone" dataKey="Y" stroke="#8884d8" />
+            <XAxis dataKey="X"/>
+            <YAxis/>
+            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <h5 className="text-center pb-4">
+        Содержание полезного компонента
+      </h5>
+    </>
+  );
+};
